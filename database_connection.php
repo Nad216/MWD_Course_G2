74ee10a4732ffae94ee07516a0f7f3a0a7c420bb
Nadun Avishka <navishka123@gmail.com>
2022-09-12 11:59:53 +0530

    Website name and connection update

--- database_connection.php
+++ database_connection.php
@@ -1,6 +1,7 @@
 <?php
-//Create Connection
+//connection after uploading to web
+//$con = mysqli_connect("sql202.unaux.com","unaux_32573806","SLH@1234");
 $con = mysqli_connect("localhost","root","");
 //select db
-mysqli_select_db($con,"tuexport");
+mysqli_select_db($con,"unaux_32573806_Hotel_reser");
 ?>
