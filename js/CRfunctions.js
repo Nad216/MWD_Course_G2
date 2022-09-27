4d355d57b0f60cd74782303907090d9ec310a7e9
Nadun Avishka <navishka123@gmail.com>
2022-09-28 00:50:31 +0530

    auto done

--- js/CRfunctions.js
+++ js/CRfunctions.js
@@ -3,3 +3,15 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll", function() {
 	header.classList.toggle ("sticky", window.scrollY > 0);
 });
+
+/*Review Automatic Navigation Start*/
+
+var countrv = 1;
+setInterval(function(){
+	document.getElementById('radio' + countrv).checked = true;
+	countrv++;
+	if(countrv > 10)
+		countrv = 1	
+},1500)
+
+/*Review Automatic Navigation Start*/
