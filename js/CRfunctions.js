19241729f0f95725e3392fb3c1338652b8785778
Nadun Avishka <navishka123@gmail.com>
2022-09-27 06:12:23 +0530

    New1

--- js/CRfunctions.js
+++ js/CRfunctions.js
@@ -1,26 +1,5 @@
-function setvisibility()
-{
-    
-    if(document.getElementById('RbSearch_id').checked == true){
-    document.getElementById('IdSelect').style.visibility = "visible";
-    document.getElementById('NicSelect').style.visibility = "hidden";
-    document.getElementById('NameSelect').style.visibility = "hidden";
-    }
-    else if(document.getElementById('RbSearch_nic').checked == true)
-    {
-    document.getElementById('IdSelect').style.visibility = "hidden";
-    document.getElementById('NicSelect').style.visibility = "visible";
-    document.getElementById('NameSelect').style.visibility = "hidden";
-    }else if(document.getElementById('RbSearch_name').checked == true)
-    {
-    document.getElementById('IdSelect').style.visibility = "hidden";
-    document.getElementById('NicSelect').style.visibility = "hidden";
-    document.getElementById('NameSelect').style.visibility = "visible";
-    }else
-    {
-    document.getElementById('IdSelect').style.visibility = "hidden";
-    document.getElementById('NicSelect').style.visibility = "hidden";
-    document.getElementById('NameSelect').style.visibility = "hidden";
-    }
-  
-}
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 0);
+});
